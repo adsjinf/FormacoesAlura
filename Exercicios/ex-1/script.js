7edfd90ad1c976863cabbeb2718026ec6932c8a8
adsjinf <jinfrj@hotmail.com>
2025-06-22 13:03:05 -0300

    Implementando solicitações de checkboc ao exercicio 1

--- Exercicios/ex-1/script.js
+++ Exercicios/ex-1/script.js
@@ -1,7 +1,10 @@
 // Pega os elementos
 const inputTarefa = document.getElementById('input-tarefa');
-const formTarefa   = document.getElementById('form-tarefa');  // mais fácil lidar com o form inteiro
+const formTarefa   = document.getElementById('form-tarefa'); 
+// mais fácil lidar com o form inteiro
 const listaTarefas = document.getElementById('lista-tarefas');
+
+let contadorId = 1;
 
 // Escuta o “enviar” do formulário (clique no botão ou Enter no input)
 formTarefa.addEventListener('submit', function (evento) {
@@ -15,9 +18,25 @@ formTarefa.addEventListener('submit', function (evento) {
 
     // Cria o <li> e joga na lista
     const li = document.createElement('li');
-    li.textContent = texto;
+    // li.textContent = texto;
+    
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = `tarefa-${contadorId++}`;
+    
+    // Cria o label com texto da tarega
+    const label = document.createElement('label');
+    label.htmlFor = checkbox.id;
+    label.textContent = texto;
+    
+    // Adiciona checkbox + label dentro do <li>
+    li.appendChild(checkbox);
+    li.appendChild(label);
+    
+    // Aciciona o <li> na lista
     listaTarefas.appendChild(li);
 
+    // Limpa e foca o input
     inputTarefa.value = '';               // limpa o input pro próximo item
     inputTarefa.focus();                  // já deixa o cursor pronto
 });
